b3b0d0405644ac03eaa94b78e5a4cc5977c9c5dd
ankitx1985 <ankitsharma20045@gmail.com>
2025-09-03 10:54:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,10 +17,10 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = os.getenv("API_ID", "26994377")
-API_HASH = os.getenv("API_HASH", "9c9eb74a4a0a1ecd4c96abebf3c637ee")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8287122600:AAEYa9CSs1MF4Rsi48CsfDdXJEhs64P2YX0")
-MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://fixmayart834:FMWwXBd4JJYMs2Iv@cluster0.ltpube9.mongodb.net/?retryWrites=true&w=majority")
+API_ID = os.getenv("API_ID", "")
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+MONGO_DB = os.getenv("MONGO_DB", "")
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "8184789731").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegramkibot")
 STRING = os.getenv("STRING", None) # optional
